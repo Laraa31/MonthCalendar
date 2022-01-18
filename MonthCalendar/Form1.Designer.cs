4c9122dcc3f0c88b961ee178f34fd7c97d046c96
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-01-18 13:00:32 +0100

    Add files via upload

---
+++ MonthCalendar/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MonthCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monCal = new System.Windows.Forms.MonthCalendar();
+            this.btnTermin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monCal
+            // 
+            this.monCal.Location = new System.Drawing.Point(67, 58);
+            this.monCal.MaxSelectionCount = 14;
+            this.monCal.Name = "monCal";
+            this.monCal.TabIndex = 0;
+            this.monCal.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monCal_DateChanged);
+            // 
+            // btnTermin
+            // 
+            this.btnTermin.Location = new System.Drawing.Point(100, 232);
+            this.btnTermin.Name = "btnTermin";
+            this.btnTermin.Size = new System.Drawing.Size(139, 32);
+            this.btnTermin.TabIndex = 1;
+            this.btnTermin.Text = "Odaberi termin";
+            this.btnTermin.UseVisualStyleBackColor = true;
+            this.btnTermin.Click += new System.EventHandler(this.btnTermin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 323);
+            this.Controls.Add(this.btnTermin);
+            this.Controls.Add(this.monCal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monCal;
+        private System.Windows.Forms.Button btnTermin;
+    }
+}
+
---
+++ MonthCalendar/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monCal_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void btnTermin_Click(object sender, EventArgs e)
+        {
+            TimeSpan period=monCal.SelectionEnd- monCal.SelectionStart;
+
+            if (period.Days < 1)
+            {
+                MessageBox.Show("Niste odabrali termin, odaberite termin pomoću tipke SHIFT!",
+                               "Greška!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Rezervirali ste period od " + 
+                    monCal.SelectionStart.ToShortDateString()+ " do "
+                    + monCal.SelectionEnd.ToShortDateString()+".",
+                    "Rezervacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+
+        }
+    }
+}
